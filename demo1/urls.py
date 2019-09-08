7ed571b47fd1a3a131e70a2f749ec48ac0a77785
lml <1131975059@qq.com>
2019-09-08 21:59:58 +0800

    1

--- demo1/urls.py
+++ demo1/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,re_path
 from . import view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('index/', view.index),
     path('hello/', view.hello),
+    re_path('^$',view.index)
 ]
--- demo1/view.py
+++ demo1/view.py
@@ -3,5 +3,3 @@ def index(request):
     return HttpResponse('hello world')
 def hello(request):
     return HttpResponse('哈哈哈哈哈')
-def add():
-    return 'nihapo'
